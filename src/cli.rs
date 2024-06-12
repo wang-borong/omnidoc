90be04c5b0da2d9a9b3d2121a4f849543ac6ce67
Jason Wang <wangborong@cdjrlc.com>
2024-06-12 23:02:21 +0800

    Update cli scheme

--- src/cli.rs
+++ src/cli.rs
@@ -1,58 +1,70 @@
 use clap::{Parser, Subcommand};
-use std::fmt;
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// set project name
-    #[arg(short, long)]
-    project: String,
-    /// set author name
-    #[arg(short, long)]
-    author: Option<String>,
-    /// set document version
-    #[arg(short, long)]
-    docver: Option<String>,
-    /// set release name
-    #[arg(short, long)]
-    release: Option<String>,
-    /// set language
-    #[arg(short, long)]
-    language: Option<String>,
-    /// set suffix
-    #[arg(short, long)]
-    suffix: Option<String>,
+//
+// Create a git-like cli program to manage our document project.
+//
 
-    /// create makefile
-    #[arg(long, default_value_t = false)]
-    makefile: bool,
-
-    //#[arg(short, long, default_value_t = 1)]
-    //count: u8,
+/// the omnidoc management cli
+#[derive(Debug, Parser)]
+#[command(name = "omnidoc")]
+#[command(version, about = "the omnidoc management cli", long_about = None)]
+struct OmniCli {
+    /// document management subcommands
+    #[command(subcommand)]
+    command: Commands,
 }
 
-impl fmt::Debug for Args {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "project: {}\n\
-            author: {}\n\
-            docver: {}\n\
-            release: {}\n\
-            language: {}\n\
-            suffix: {}\n\
-            makefile: {}"
-            , self.project
-            , self.author.as_ref().unwrap_or(&"None".to_string())
-            , self.docver.as_ref().unwrap_or(&"None".to_string())
-            , self.release.as_ref().unwrap_or(&"None".to_string())
-            , self.language.as_ref().unwrap_or(&"None".to_string())
-            , self.suffix.as_ref().unwrap_or(&"None".to_string())
-            , self.makefile
-        )
-    }
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// create a new project
+    Create {
+        /// set project name
+        #[arg(short, long)]
+        project: String,
+        /// set author name
+        #[arg(short, long)]
+        author: Option<String>,
+        /// set document version
+        #[arg(short, long)]
+        docver: Option<String>,
+        /// set release name
+        #[arg(short, long)]
+        release: Option<String>,
+        /// set language
+        #[arg(short, long)]
+        language: Option<String>,
+        /// set suffix
+        #[arg(short, long)]
+        suffix: Option<String>,
+
+        /// create makefile
+        #[arg(long, default_value_t = false)]
+        makefile: bool,
+    },
+
+    /// build the document project
+    Build {
+        /// path to documentation source files
+        source: Option<String>, 
+        /// path to output directory
+        output: Option<String>,
+
+        /// builder to use (default to 'pdf')
+        #[arg(short, long)]
+        builder: Option<String>,
+        /// run in parallel with N processes, when possible.
+        #[arg(short, long, value_name = "N")]
+        jobs: Option<u8>,
+    },
+
+    /// clean the document project
+    Clean {
+
+    },
 }
 
 pub fn cli() {
-    let args = Args::parse();
+    let args = OmniCli::parse();
 
     println!("{:?}", args);
 }
